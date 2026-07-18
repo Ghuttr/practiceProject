768a843768d9e1d058cb1d98fc21ebe9f9f94945
Ghuttr <120268965+Ghuttr@users.noreply.github.com>
2026-07-18 21:05:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 GOOGLE_CLIENT_ID = '868845313562-7nb6cqidtiticpqpafjrjmh3p5j7qps7.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = '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'
-SERVER_NAME = '147.45.141.180'
+SERVER_NAME = 'mywerse.ru'
